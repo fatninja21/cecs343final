df0112eed1bc2024187cf73c96bcdf84f9f75e08
fatninja21 <fatninja2118@gmail.com>
2019-11-26 12:57:07 -0800

    testing 123 123

--- src/test/newFrame.java
+++ src/test/newFrame.java
@@ -2381,6 +2381,7 @@ public void displayMainMenu(){
 		double comission = Double.parseDouble(editSlsPersonCommissionTf.getText());
 		int id = Integer.parseInt(displayCurrentSlsPersonIDLbl.getText());
 		sales.get(index).editSalesPerson(fName, lName, comission, id);
+                System.out.println("pooop");
 	}
 	
 	public void addSalesperson() {
